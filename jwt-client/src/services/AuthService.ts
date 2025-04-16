c29d1e5379a2160a67714a902b9a28542bc2d891
Saveliy Dmitriyev <saveliy@Saveliys-MacBook-Air.local>
2025-04-16 22:40:34 +0500

    Add tanstack query and api,hooks files

--- jwt-client/src/services/AuthService.ts
+++ jwt-client/src/services/AuthService.ts
@@ -1,17 +1,19 @@
-import { AxiosResponse } from "axios";
 import $api from "../http";
 import { AuthResponse } from "../models/response/AuthResponse";
 
 export default class AuthService {
-    static async signIn(email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
-        return $api.post<AuthResponse>('/signIn', { email, password});
+    static async signIn(email: string, password: string): Promise<AuthResponse> {
+        const res = await $api.post<AuthResponse>('/signIn', { email, password});
+        return res.data;
     }
 
     static async signOut(): Promise<void> {
         return $api.post('/signOut');
     }
 
-    static async signUp(email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
-        return $api.post<AuthResponse>('/signUp', { email, password});
+    static async signUp(email: string, password: string): Promise<AuthResponse> {
+        const res = await $api.post<AuthResponse>('/signUp', { email, password});
+        return res.data;
     }
+
 }
