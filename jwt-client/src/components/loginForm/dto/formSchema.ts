b5776519f46c475a61d4339883495072151cc56f
Saveliy Dmitriyev <saveliy@Saveliys-MacBook-Air.local>
2025-04-16 22:08:23 +0500

    Add separate signIn and signUp schemas, add actionType type

--- jwt-client/src/components/loginForm/dto/formSchema.ts
+++ jwt-client/src/components/loginForm/dto/formSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-const FormSchema = z.object({
+const SignUpSchema = z.object({
     username: z.string().min(3, {
       message: "Username must be at least 2 characters",
     }),
@@ -20,4 +20,27 @@ const FormSchema = z.object({
         }),
 })
 
-export default FormSchema;
+const SignInSchema = z.object({
+    username: z.string().min(3, {
+      message: "Username must be at least 2 characters",
+    }),
+    email: z
+        .string()
+        .min(1, {
+            message: "Email is required.",
+        })
+        .email({ message: 'Entera valid email address'}),
+    password: z
+        .string()
+        .min(6, {
+            message: "Password must be at least 6 characters long.",
+        })
+        .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()[\]{};:'",.<>/?\\|`~\-_=+]).{6,}$/, {
+            message: 'Password must include at least one uppercase letter, one number, and one special character'
+        }),
+})
+
+export {
+    SignInSchema,
+    SignUpSchema,
+};
