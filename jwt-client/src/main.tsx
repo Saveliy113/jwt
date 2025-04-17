4c39561a1da66c24cd9f4d24dba97ae4dab3bbb9
Saveliy Dmitriyev <saveliy@Saveliys-MacBook-Air.local>
2025-04-17 23:25:32 +0500

    Wrap app in queryClientProvider

--- jwt-client/src/main.tsx
+++ jwt-client/src/main.tsx
@@ -2,17 +2,21 @@ import { StrictMode } from 'react';
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { Toaster } from "@/components/ui/sonner"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import router from './router/router';
 
 import './index.css';
 
 const root: HTMLElement = document.getElementById('root') as HTMLElement;
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(root).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>
 );
 
